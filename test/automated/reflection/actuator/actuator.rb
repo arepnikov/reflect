f228c2969cfd556ab67891c01989b49dbeaf6806
Alexander Repnikov <a.repnikov@visuality.pl>
2024-09-14 20:15:47 +0200

    Actuator and Variant Actuator test clarified

--- test/automated/reflection/actuator/actuator.rb
+++ test/automated/reflection/actuator/actuator.rb
@@ -3,19 +3,17 @@ require_relative '../../automated_init'
 context "Reflect" do
   context "Reflection" do
     context "Actuator" do
-      subject = Reflect::Controls::Subject::Example.new
-      target = Reflect::Controls::Subject::Example::SomeConstant
+      constant_name = Controls::Namespace::Random.get
+      subject = Controls::Subject.example
+      target = Controls::Target.example(constant_name:, subject:)
 
-      constant_name = :SomeConstant
       method_name = :some_method
-
       RecordInvocation.(target, method_name)
 
       reflection = Reflect.(subject, constant_name, strict: true)
       reflection.(method_name, subject, :some_arg)
 
-      invocations = target.invocations(method_name)
-      invocation = invocations.last
+      invocation = target.invocation(method_name)
 
       context "Target's #{method_name} Method" do
         test! "Invoked" do
