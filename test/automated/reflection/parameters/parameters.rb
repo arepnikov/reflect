b8d4e59b7a176c816b6927f4247aa07072eb1aad
Alexander Repnikov <a.repnikov@visuality.pl>
2024-09-14 20:16:10 +0200

    Parameters test clarified

--- test/automated/reflection/parameters/parameters.rb
+++ test/automated/reflection/parameters/parameters.rb
@@ -3,15 +3,14 @@ require_relative '../../automated_init'
 context "Reflect" do
   context "Reflection" do
     context "Parameters" do
-      constant_name = :SomeConstant
+      constant_name = Controls::Namespace::Random.get
+      subject = Controls::Subject.example
+      target = Controls::Target::MixedParameters.example(constant_name:, subject:)
+
       method_name = :some_method
-
-      subject = Reflect::Controls::Subject::MixedParameters::Example.new
-
-      target = Reflect::Controls::Subject::MixedParameters::Example::SomeConstant
       target_method_parameters = target.method(method_name).parameters
 
-      reflection  = Reflect.(subject, constant_name, strict: true)
+      reflection = Reflect.(subject, constant_name, strict: true)
       parameters = reflection.parameters(method_name)
 
       test "Target method's parameters" do
